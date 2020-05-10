79d01c3cdea832a954d2d8650feaa1a9006fc863
zarias2020 <zarias@yandex.ru>
2020-05-10 19:11:23 +0300

    added slider

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+
+
+  $('.slider__inner').slick({
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: false,
+    nextArrow: false,
+    draggable: true,
+    easing: true,
+    cssEase: 'linear',
+  });
+
+
+
+
+
+
+
+
+
+
+});
